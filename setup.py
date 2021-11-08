e8e488a68d043a941907ef7fc1ec1d56fa547a81
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2021-11-08 18:36:29 -0300

    long_description field added to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import find_packages, setup
 setup(
     name='mt5linux',
     packages=find_packages(include=['mt5linux']),
-    version='0.1.0',
+    version='0.1.2',
     description='MetaTrader5 for linux users',
+    long_description=open('README.md','r').read(),
+    long_description_content_type='text/markdown',
     author='Lucas Prett Campagna',
     license='MIT',
     url = 'https://github.com/lucas-campagna/mt5linux',
