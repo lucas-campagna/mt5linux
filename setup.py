c70d9bfbc903806b7451af5a52241d988d616fe8
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2021-11-08 18:39:16 -0300

    version was wrong

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mt5linux',
     packages=find_packages(include=['mt5linux']),
-    version='0.1.2',
+    version='0.1.3',
     description='MetaTrader5 for linux users',
     long_description=open('README.md','r').read(),
     long_description_content_type='text/markdown',
