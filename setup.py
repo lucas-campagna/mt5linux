36a4967c36e091098c12f888f7a8be37836df2a9
Henrique Perez de Andrade <hpdeandrade@gmail.com>
2025-04-30 08:51:33 -0300

    Update project files

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
 setup(
-    name='mt5linux',
-    packages=find_packages(include=['mt5linux']),
-    version='0.2.0',
-    description='MetaTrader5 for linux users',
-    long_description=open('README.md','r').read(),
-    long_description_content_type='text/markdown',
-    author='Lucas Prett Campagna',
-    license='MIT',
-    url = 'https://github.com/lucas-campagna/mt5linux',
-    install_requires=open('requirements.txt','r').read().split('\n'),
-    setup_requires=[],
-    tests_require=[],
-    test_suite='tests',
+    name="mt5linux",
+    version="0.2.0",
+    author="Lucas Prett Campagna",
+    author_email="lucasprett129@gmail.com",
+    description="MetaTrader5 for Linux users",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lucas-campagna/mt5linux",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.13",
+    install_requires=[
+        "rpyc"
+    ]
 )
