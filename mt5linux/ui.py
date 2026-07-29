11be78745ecd24ea5f45efea1e8f16480aed944a
Lucas Prett Campagna <lucasprett129@gmail.com>
2026-07-29 18:15:58 -0300

    add UI automation helper class

---
+++ mt5linux/ui.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class UI:
+    def __init__(self, conn):
+        self.__conn = conn
+
+    def search_server(self, server):
+        if not server:
+            logging.error("Server not provided")
+            return
+        self.__conn.execute(f'open("C:\\\\server", "w").write("{server}")')
